f7f2b8cf12968a07e09940167a1db2f0ff8837dc
You Guang <yokurang@gmail.com>
2023-08-04 01:29:01 +0700

    modify details

--- src/app/data/projects/projects.ts
+++ src/app/data/projects/projects.ts
@@ -10,10 +10,10 @@ export type Project = {
 export const projects: Project[] = [
   {
       title: 'yokurang.com',
-      description: 'My personal website',
+      description: 'The website you are currently on!',
       projectLink: 'https://www.yokurang.com',
       codeLink: 'https://github.com/yokurang/yokurang.com',
-      tags: ['React', 'TypeScript', 'AWS'],
-      details: 'Built using React TypeScript and hosted using AWS.'
+      tags: ['React', 'TypeScript', 'CI/CD'],
+      details: 'Built using Next.js, React, TypeScript, and Tailwind CSS. CI/CD pipeline using GitHub Actions.'
   },
 ];
