7443acf409f12c87b0fcbb02e73801ab6be320ff
You Guang <yokurang@gmail.com>
2023-08-28 14:11:56 +0800

    fix overflow issue

--- src/app/components/resume/Skills.tsx
+++ src/app/components/resume/Skills.tsx
@@ -18,9 +18,9 @@ const Skills = ({ data }: { data: TSkill[] }) => {
           <h3 className={`font-bold text-lg md:text-xl ${categoryColor[category]}`}>{category}</h3>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
             {categories[category]?.map((skill, index) => (
-              <div key={index} className="py-2 px-4 bg-white rounded-md flex items-center gap-2 mb-2 border border-gray-200">
-                <span className="font-bold text-sm md:text-base">{skill.name}</span>
-                <span>({skill.level})</span>
+              <div key={index} className="py-2 px-4 bg-white rounded-md flex flex-wrap items-center gap-2 mb-2 border border-gray-200">
+                <span className="font-bold text-sm md:text-base break-words">{skill.name}</span>
+                <span className="break-normal">({skill.level})</span>
               </div>
             ))}
           </div>
